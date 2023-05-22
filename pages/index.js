715967f7752c493359c86ec8922a4448247eccc5
amalieqw7 <amalieqw.21@ichat.sp.edu.sg>
2023-05-22 23:30:00 +0800

    hompage edits

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <div>
-            <h1 className='PageHeader'>Hello this is homepage</h1>
-            <p>test test</p>
+            <h1 className='PageHeader'>Hello this is the Homepage</h1>
+            <p>Check back here in Phase 2!</p>
         </div>
     )
 }
