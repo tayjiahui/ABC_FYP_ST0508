25995b2d5cfef14b5698dc3bc3b9fb928bf864c0
amalieqw7 <amalieqw.21@ichat.sp.edu.sg>
2023-07-12 03:27:15 +0800

    Temporary blanked out calendar for deployment

--- pages/PurchasePlanning/index.js
+++ pages/PurchasePlanning/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import CalendarDisplay from './Calendar'
 
-document.addEventListener('DOMContentLoaded', function() {
-  createRoot(document.body.appendChild(document.createElement('div')))
-    .render(<CalendarDisplay />)
-})
+// document.addEventListener('DOMContentLoaded', function() {
+//   createRoot(document.body.appendChild(document.createElement('div')))
+//     .render(<CalendarDisplay />)
+// })
+
+export default function Index() {
+  return (
+      <div></div>
+  );
+};
