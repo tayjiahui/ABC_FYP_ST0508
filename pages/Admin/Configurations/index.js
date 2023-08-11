81a63cfd0a009b57e80baf995e3184702db8407c
Jia Hui <tayjiahui1@gmail.com>
2023-08-11 22:34:36 +0800

    #139, Delete Purchase & Payment status

--- pages/Admin/Configurations/index.js
+++ pages/Admin/Configurations/index.js
@@ -103,13 +103,162 @@ function PaymentModePageView() {
     )
 };
 
+function StatusPageView() {
+
+    const [paymentStatuses, setPaymentStatuses] = useState([]);
+    const [paymentStatusID, setPaymentStatusID] = useState([]);
+
+    const fetchPaymentStatuses = () => {
+        axios.get(`${baseUrl}/api/paymentTrack/`)
+            .then(response => {
+                setPaymentStatuses(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching payment statuses:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchPaymentStatuses();
+    }, []);
+
+    const handleDelete = (status) => {
+        axios.get(`${baseUrl}/api/paymentTrack/status/${status}`)
+            .then(response => {
+                const statusID = response.data[0].PaymentStatusID
+                setPaymentStatusID(statusID);
+
+                axios.delete(`${baseUrl}/api/paymentTrack/${statusID}`)
+                    .then(response => {
+                        console.log('status deleted!')
+                        fetchPaymentStatuses();
+                    })
+                    .catch(err => {
+                        console.log('status could not be deleted', err)
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
+    return (
+        <>
+            <div className="p-4">
+                <h2>Payment Status</h2>
+            </div>
+
+            <div className="px-4">
+                <div className="d-flex">
+                    <label><b>Manage Statuses</b></label>
+                </div>
+
+                <div className="pb-3 mt-3">
+                    <ul style={{ listStyleType: 'none', padding: 0 }}>
+                        {paymentStatuses.map((status, index) => (
+                            <li key={index} className="d-flex align-items-center">
+                                <span>{status.paymentStatus}</span>
+                                {['Pending', 'Payment Sent', 'Payment Received'].includes(status.paymentStatus) ? null : (
+                                    <button className="btn btn-link btn-sm" onClick={() => handleDelete(status.paymentStatus)} style={{ textDecoration: 'none', color: 'black', lineHeight: '1' }}>
+                                        <span aria-label="Delete" style={{ fontSize: '20px', verticalAlign: 'middle' }}>&times;</span>
+                                    </button>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+};
+
+function PurchaseStatusPageView() {
+
+    const [purchaseStatuses, setPurchaseStatuses] = useState([]);
+    const [purchaseStatusID, setPurchaseStatusID] = useState([]);
+
+    const fetchPurchaseStatuses = () => {
+        axios.get(`${baseUrl}/api/trackOrder/purchaseStatus/all`)
+            .then(response => {
+                const purchaseStatusesData = response.data.map(item => item.purchaseStatus);
+                setPurchaseStatuses(purchaseStatusesData);
+                console.log(purchaseStatusesData)
+            })
+            .catch(error => {
+                console.error("Error fetching purchase statuses:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchPurchaseStatuses();
+    }, []);
+
+
+    const handleDelete = (status) => {
+        alert(`${status}`)
+        axios.get(`${baseUrl}/api/trackOrder/purchaseStatus/id/${status}`)
+        .then(response => {
+            const statusID = response.data[0].purchaseStatusID
+            setPurchaseStatusID(statusID);
+
+            console.log(response)
+
+            // axios.delete(`${baseUrl}/api/trackOrder/purchaseStatus/${statusID}`)
+            // .then(response => {
+            //     console.log('status deleted!')
+            //     fetchPurchaseStatuses();
+            // })
+            // .catch(err => {
+            //     console.log('status could not be deleted', err)
+            // })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    }
+
+
+    return (
+        <>
+            <div className="p-4">
+                <h2>Purchase Status</h2>
+            </div>
+
+            <div className="px-4">
+                <div className="d-flex">
+                    <label><b>Manage Statuses</b></label>
+                </div>
+
+                <div className="pb-3 mt-3">
+                    <ul style={{ listStyleType: 'none', padding: 0 }}>
+                        {purchaseStatuses.map((status, index) => (
+                            <li key={index} className="d-flex align-items-center">
+                                <span>{status}</span>
+                                {['Accept Order', 'Preparing Order', 'Preparing Delivery', 'Shipping Item', 'Item Delivered'].includes(status) ? null : (
+                                    <button className="btn btn-link btn-sm" onClick={() => handleDelete(status)} style={{ textDecoration: 'none', color: 'black', lineHeight: '1' }}>
+                                        <span aria-label="Delete" style={{ fontSize: '20px', verticalAlign: 'middle' }}>&times;</span>
+                                    </button>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+};
+
 export default function Configurations() {
 
     const [Token, setToken] = useState();
 
     const [PageView, setPageView] = useState();
-    const [GSTPage, setGSTPage] = useState(<GSTPageView/>);
-    const [PaymentModePage, setPaymentModePage] = useState(<PaymentModePageView/>);
+    const [GSTPage, setGSTPage] = useState(<GSTPageView />);
+    const [PaymentModePage, setPaymentModePage] = useState(<PaymentModePageView />);
+    const [StatusView, setStatusView] = useState(<StatusPageView />)
+    const [PurchaseStatusView, setPurchaseStatusView] = useState(<PurchaseStatusPageView />)
 
 
     useEffect(() => {
@@ -121,12 +270,16 @@ export default function Configurations() {
     const PageDirectory = (e) => {
         const clicked = e.target.id;
 
-        if(clicked === 'GST'){
+        if (clicked === 'GST') {
             setPageView(GSTPage);
-        } else if (clicked === 'PaymentMode'){
+        } else if (clicked === 'PaymentMode') {
             setPageView(PaymentModePage);
+        } else if (clicked === 'Statuses') {
+            setPageView(StatusView)
+        } else if (clicked === 'PurchaseStatus') {
+            setPageView(PurchaseStatusView)
         }
-        
+
     };
 
     return (
@@ -146,6 +299,16 @@ export default function Configurations() {
                     <div className="row border-bottom">
                         <button onClick={PageDirectory} className="btn text-start">
                             <h5 id="PaymentMode" className="py-2 px-5">Payment Mode</h5>
+                        </button>
+                    </div>
+                    <div className="row border-bottom">
+                        <button onClick={PageDirectory} className="btn text-start">
+                            <h5 id="Statuses" className="py-2 px-5">Payment Status</h5>
+                        </button>
+                    </div>
+                    <div className="row border-bottom">
+                        <button onClick={PageDirectory} className="btn text-start">
+                            <h5 id="PurchaseStatus" className="py-2 px-5">Purchase Status</h5>
                         </button>
                     </div>
                 </div>
